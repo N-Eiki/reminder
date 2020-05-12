564d043e9ecdd925ac1ffeb6a9dfe1f807a3a01b
N-Eiki <naganagaeichan@gmail.com>
2020-05-12 22:46:04 +0900

    詳細ページについて

--- remind/urls.py
+++ remind/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import signupfunc,loginfunc, homefunc, logoutfunc
+from .views import signupfunc,loginfunc, homefunc, logoutfunc, detailfunc
 
 urlpatterns = [
     path('signup/', signupfunc, name="signup"),
     path('login/', loginfunc, name="login"),
     path('home/', homefunc, name="home"),
     path("login/", logoutfunc, name="logout"),
-
+    path('detail/<day>/<int:timetable>', detailfunc, name="detail")
 ]
