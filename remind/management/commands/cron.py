aa543fb5c78059d00d695cb1e0d0c2569f4aab8f
N-Eiki <naganagaeichan@gmail.com>
2020-05-20 15:39:44 +0900

    リマインド内容について

--- remind/management/commands/cron.py
+++ remind/management/commands/cron.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from remind.util.tool import message_create, job
+from remind.util.tool import job
 from remind.models import SubjectModel
  
 class Command(BaseCommand):
