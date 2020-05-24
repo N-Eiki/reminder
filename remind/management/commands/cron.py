d8f8af8f997ee195c238e3bd45ff5a70484957fd
N-Eiki <naganagaeichan@gmail.com>
2020-05-24 16:39:55 +0900

    cron.pyの調整

--- remind/management/commands/cron.py
+++ remind/management/commands/cron.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from remind.util.tool import job, get_token
+from remind.util.tool import job, get_token,get_message
 from remind.models import SubjectModel
 from django.contrib.auth.models import User
 
@@ -11,4 +11,3 @@ class Command(BaseCommand):
             print('------------------')
             print(u)
             job(u)
-            # print(get_token(u))
